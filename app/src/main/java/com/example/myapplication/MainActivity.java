7c7a5596f1a62456f2e14706486476b372ad8cfd
denlog1977 <denlog1977@gmail.com>
2019-10-22 16:06:48 +0300

    1

---
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -0,0 +1,39 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        final TextView textViewEmailMessage = (TextView) findViewById(R.id.textViewMessage);
+        final EditText editTextName = (EditText) findViewById(R.id.editTextName);
+        final EditText editTextEmail = (EditText) findViewById(R.id.editTextEmail);
+
+        Button buttonOK = (Button) findViewById(R.id.buttonOK);
+        buttonOK.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                textViewEmailMessage.setText("Подписка на рассылку успешно оформлена для пользователя " + editTextName.getText() + " на электронный адрес " + editTextEmail.getText());
+            }
+        });
+
+        Button buttonClear = (Button) findViewById(R.id.buttonClear);
+        buttonClear.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                textViewEmailMessage.setText("");
+            }
+        });
+
+    }
+}
